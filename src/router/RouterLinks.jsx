aeb2ee48de0c29f2c1547f845bd90545638a9e3f
olmos0901 <olmosbek9933@gmail.com>
2023-05-08 02:01:07 +0500

    5-oy_Imtihon

---
+++ src/router/RouterLinks.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Liked } from "../pages/Liked/Liked";
+import { Home } from "../pages/Home/Home";
+import { Singleproduct } from "../pages/Singleproduct/Singleproduct";
+import { Ordering } from "../pages/Ordering/Ordering";
+import { Order } from "../pages/Order/Order";
+import { ServiceRequirements } from "../pages/ServiceRequirements/ServiceRequirements";
+import { Contact } from "../pages/Contact/Contact";
+import { Cart } from "../pages/Cart/Cart";
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/liked" element={<Liked />} />
+      <Route path="/singleproduct/:id" element={<Singleproduct />} />
+      <Route path="/ordering/product/:id" element={<Ordering />} />
+      <Route path="/order/product/:id" element={<Order />} />
+      <Route path="/service-requirement" element={<ServiceRequirements />} />
+      <Route path="/contacts" element={<Contact />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
+};
